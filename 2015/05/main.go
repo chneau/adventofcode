8618affcd974aae936a4222f2b3b7878b4d29448
chneau <charles63500@gmail.com>
2020-12-03 00:33:14 +0000

    2015 > 05

--- 2015/05/main.go
+++ 2015/05/main.go
@@ -9,47 +9,79 @@ import (
 
 func main() {
 	raw := common.DownloadInput("https://adventofcode.com/2015/day/5/input")
-	vowels := []string{"a", "e", "i", "o", "u"}
-	unwanteds := []string{"ab", "cd", "pq", "xy"}
-	goodLines := 0
 	log.Println("PART ONE")
-	for _, line := range strings.Split(raw, "\n") {
-		if line == "" {
-			continue
-		}
-		vowelsCount := 0
-		for _, vowel := range vowels {
-			vowelsCount += strings.Count(line, vowel)
-			if vowelsCount == 3 {
-				break
+	{
+		vowels := []string{"a", "e", "i", "o", "u"}
+		unwanteds := []string{"ab", "cd", "pq", "xy"}
+		goodLines := 0
+		for _, line := range strings.Split(raw, "\n") {
+			if line == "" {
+				continue
 			}
-		}
-		if vowelsCount < 3 {
-			continue
-		}
-		hasDouble := false
-		for i := 0; i < len(line)-1; i++ {
-			if line[i] == line[i+1] {
-				hasDouble = true
-				break
+			vowelsCount := 0
+			for _, vowel := range vowels {
+				vowelsCount += strings.Count(line, vowel)
+				if vowelsCount == 3 {
+					break
+				}
 			}
-		}
-		if hasDouble == false {
-			continue
-		}
-		hasForbiden := false
-		for _, unwanted := range unwanteds {
-			if strings.Contains(line, unwanted) {
-				hasForbiden = true
-				break
+			if vowelsCount < 3 {
+				continue
 			}
+			hasDouble := false
+			for i := 0; i < len(line)-1; i++ {
+				if line[i] == line[i+1] {
+					hasDouble = true
+					break
+				}
+			}
+			if hasDouble == false {
+				continue
+			}
+			hasForbiden := false
+			for _, unwanted := range unwanteds {
+				if strings.Contains(line, unwanted) {
+					hasForbiden = true
+					break
+				}
+			}
+			if hasForbiden {
+				continue
+			}
+			goodLines++
 		}
-		if hasForbiden {
-			continue
-		}
-		log.Println(vowelsCount, hasDouble, hasForbiden)
-		goodLines++
-		log.Println(line)
+		log.Println("Good lines:", goodLines)
 	}
-	log.Println("Good lines:", goodLines)
+	log.Println("PART TWO")
+	{
+		goodLines := 0
+		for _, line := range strings.Split(raw, "\n") {
+			hasDouble := false
+			doubles := ""
+			for i := 0; i < len(line)-2; i++ {
+				if strings.Count(line, line[i:i+2]) >= 2 {
+					hasDouble = true
+				}
+			}
+			if hasDouble == false {
+				continue
+			}
+			if strings.Count(line, doubles) < 2 {
+				continue
+			}
+			hasWeirdDouble := false
+			for i := 0; i < len(line)-2; i++ {
+				if line[i] == line[i+2] {
+					hasWeirdDouble = true
+					break
+				}
+			}
+			if hasWeirdDouble == false {
+				continue
+			}
+
+			goodLines++
+		}
+		log.Println("Good lines:", goodLines)
+	}
 }
